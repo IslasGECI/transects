866d3f80006df9656471dbd64df4291eb197620d
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2023-03-16 10:08:35 -0700

    ✅🧪 Pass test for 'calculate_rabbit_population_percentiles()'

--- tests/test_population_estimate.py
+++ tests/test_population_estimate.py
@@ -1,16 +1,50 @@
-from transects import calculate_rabbit_population_percentils
+from transects import (
+    calculate_rabbit_population_percentils,
+    calculate_population_estimate_by_season_and_transect,
+    write_estimations,
+)
 
+import os
 import pandas as pd
 
 
+transects_data = pd.read_csv("tests/data/transectos_conejos_isla_clarion.csv")
+expected_2022_percentiles = {
+    "min": 882.1903270242863,
+    "medium": 1520.9899638266606,
+    "max": 2947.4381238658193,
+}
+
+
 def test_write_rabbit_population_estimate():
-    transects_data = pd.read_csv("tests/data/transectos_conejos_isla_clarion.csv")
-    obtained_percentiles =calculate_rabbit_population_percentils(transects_data)
+    obtained_percentiles = calculate_rabbit_population_percentils(transects_data)
     expected_percentiles = {
         "min": 1128.6071615937835,
-        "max": 3180.1537328618992,
-        "medium": 1128.6071615937835
-        }
+        "max": 2974.91369532452,
+        "medium": 1128.6071615937835,
+    }
     assert obtained_percentiles[0] == expected_percentiles["min"]
     assert obtained_percentiles[1] == expected_percentiles["medium"]
     assert obtained_percentiles[2] == expected_percentiles["max"]
+
+    season = 2022
+    obtained_2022_percentiles = calculate_rabbit_population_percentils(transects_data, season)
+    assert obtained_2022_percentiles[0] == expected_2022_percentiles["min"]
+    assert obtained_2022_percentiles[1] == expected_2022_percentiles["medium"]
+    assert obtained_2022_percentiles[2] == expected_2022_percentiles["max"]
+
+
+def test_calculate_population_estimate_by_season_and_transect():
+    season = 2022
+    obtained = calculate_population_estimate_by_season_and_transect(transects_data, season)
+    obtained_length = len(obtained)
+    expected_length = 4
+    assert obtained_length == expected_length
+
+
+def test_write_estimations():
+    output_path = "tests/data/rabbit_estimation.json"
+    intervals = list(expected_2022_percentiles.values())
+    print(intervals)
+    write_estimations(output_path, intervals)
+    assert os.path.exists(output_path)
--- transects/population_estimate.py
+++ transects/population_estimate.py
@@ -2,20 +2,43 @@ from geci_distance import GECI_Distance
 from bootstrapping_tools import boostrapping_feature
 
 import numpy as np
+import json
 
-def calculate_rabbit_population_percentils(transects_data):
+
+def write_estimations(output_path, intervals):
+    with open(output_path, "w") as exit_file:
+        json.dump(
+            {
+                "minimo": f"{np.around(intervals[1] - intervals[0]):,.0f}",
+                "media": f"{np.around(intervals[1]):,.0f}",
+                "maximo": f"{np.around(intervals[2] - intervals[1]):,.0f}",
+            },
+            exit_file,
+        )
+
+
+def calculate_rabbit_population_percentils(transects_data, season=2021):
+    population_estimations = calculate_population_estimate_by_season_and_transect(
+        transects_data, season
+    )
+    estimation_bootstrap = boostrapping_feature(population_estimations)
+    return get_confidence_interval(estimation_bootstrap)
+
+
+def get_confidence_interval(estimation_bootstrap):
+    minimum_value = np.percentile(estimation_bootstrap, 2.5)
+    medium_value = np.percentile(estimation_bootstrap, 50)
+    maximum_value = np.percentile(estimation_bootstrap, 97.5)
+    return [minimum_value, medium_value, maximum_value]
+
+
+def calculate_population_estimate_by_season_and_transect(transects_data, season=2021):
     population_estimations = []
-    transects_data = transects_data[transects_data["Temporada"] == 2021]
+    transects_data = transects_data[transects_data["Temporada"] == season]
     for _, data in transects_data.groupby(by="Transecto"):
         project = GECI_Distance(data.Cantidad_individuos, data.Distancia)
         project.set_line_width(100)
         project.set_line_length(1000)
         project.set_study_area(data.Area_isla.iloc[0])
         population_estimations.append(project.estimate_population())
-
-    estimation_bootstrap = boostrapping_feature(population_estimations)
-
-    minimum_value = np.percentile(estimation_bootstrap, 2.5)
-    medium_value = np.percentile(estimation_bootstrap, 50)
-    maximum_value = np.percentile(estimation_bootstrap, 97.5)
-    return [minimum_value, medium_value, maximum_value]
+    return population_estimations
