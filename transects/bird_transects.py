c8b390913dddea9ed187a6e3f482d0318b4b93c1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-21 20:02:41 +0000

    ♻️  Refactor

--- transects/bird_transects.py
+++ transects/bird_transects.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def get_transect_area(transects_info):
     areas = {
         "MMAA": 0,
@@ -11,7 +14,7 @@ def get_transect_area(transects_info):
 
 
 def calculate_transect_area(transects_info, transect_key):
-    area_differentials = {"MMAA": 60, "MMAB": 3.1416 * (30) ** 2, "MMAD": 60}
+    area_differentials = {"MMAA": 60, "MMAB": np.pi * (30) ** 2, "MMAD": 60}
     return (
         transects_info[transects_info.clave_muestreo == transect_key].longitud_transecto.values
         * area_differentials[transect_key]
