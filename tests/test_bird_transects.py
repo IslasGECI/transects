94c23542ebda3ed2958afcf073cbc1f4abe4c8cc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-21 21:46:40 +0000

    ✅ Pass tests

--- tests/test_bird_transects.py
+++ tests/test_bird_transects.py
@@ -1,16 +1,18 @@
 import pandas as pd
 from pytest import approx
 
-from transects import get_transect_area
+from transects import get_transect_area, get_transect_length
+
+
+transect_dict = {
+    "clave_muestreo": ["MMAA", "MMAB", "MMAB", "MMAC", "MMAD"],
+    "longitud_transecto": [100, 200, 200, 400, 300],
+    "punto_transecto": ["T1", "1", "2", "NA", "T2"],
+}
+transect_df = pd.DataFrame(transect_dict)
 
 
 def test_get_transect_area():
-    transect_dict = {
-        "clave_muestreo": ["MMAA", "MMAB", "MMAB", "MMAC", "MMAD"],
-        "longitud_transecto": [100, 200, 200, 400, 300],
-        "punto_transecto": ["T1", "1", "2", "NA", "T2"],
-    }
-    transect_df = pd.DataFrame(transect_dict)
     obtained = get_transect_area(transect_df)
     expected_len = 3
     assert len(obtained) == expected_len
--- transects/bird_transects.py
+++ transects/bird_transects.py
@@ -15,8 +15,12 @@ def get_transect_area(transects_info):
 
 def calculate_transect_area(transects_info, transect_key):
     transect_length = get_transect_length(transects_info, transect_key)
-    area_differentials = {"MMAA": 60, "MMAB": (np.pi * (30) ** 2) / transect_length, "MMAD": 60}
-    return transect_length.sum() * area_differentials[transect_key]
+    area_differentials = {"MMAA": 60, "MMAB": (np.pi * (30) ** 2), "MMAD": 60}
+    if transect_key != "MMAB":
+        return transect_length.sum() * area_differentials[transect_key]
+    else:
+        number_points = len(transect_length)
+        return number_points * area_differentials[transect_key]
 
 
 def get_transect_length(transects_info, transect_key):
