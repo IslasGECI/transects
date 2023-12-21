49d69d0f5d0f6efd8286ba0de15990c1f2829afe
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-21 21:49:08 +0000

    ♻️  Refactor

--- transects/bird_transects.py
+++ transects/bird_transects.py
@@ -17,7 +17,7 @@ def calculate_transect_area(transects_info, transect_key):
     transect_length = get_transect_length(transects_info, transect_key)
     area_differentials = {"MMAA": 60, "MMAB": (np.pi * (30) ** 2), "MMAD": 60}
     if transect_key != "MMAB":
-        return transect_length.sum() * area_differentials[transect_key]
+        return transect_length * area_differentials[transect_key]
     else:
         number_points = len(transect_length)
         return number_points * area_differentials[transect_key]
