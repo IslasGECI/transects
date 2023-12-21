ad7bb63c7080afb997744bf82730d25f8d4dcd89
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-21 19:46:41 +0000

    ♻️  Refactor

--- transects/bird_transects.py
+++ transects/bird_transects.py
@@ -4,10 +4,15 @@ def get_transect_area(transects_info):
         "MMAB": "",
         "MMAD": "",
     }
-    areas["MMAA"] = (
-        transects_info[transects_info.clave_muestreo == "MMAA"].longitud_transecto.values * 60
-    )
+    transect_key = "MMAA"
+    areas[transect_key] = calculate_transect_area(transects_info, transect_key)
     areas["MMAD"] = (
         transects_info[transects_info.clave_muestreo == "MMAD"].longitud_transecto.values * 60
     )
     return areas
+
+
+def calculate_transect_area(transects_info, transect_key):
+    return (
+        transects_info[transects_info.clave_muestreo == transect_key].longitud_transecto.values * 60
+    )
