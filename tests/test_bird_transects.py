3723840c1275121821cc007575e0a61b7ff9871c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-12-22 19:12:31 +0000

    🛑🧪 Fail tests

--- tests/test_bird_transects.py
+++ tests/test_bird_transects.py
@@ -84,9 +84,10 @@ def test_count_species_by_method():
         [["MMAA", "MMAB", "MMAA"], ["species 2", "species 1", "species 1"]],
         names=["clave_muestreo", "Especie"],
     )
-    print(grouped_data.loc[(slice(None), ["species 1"]), :])
-    print(grouped_data.xs("species 1", level="Especie"))
+    print(grouped_data.index.get_level_values("Especie").unique())
     obtained = count_total_individuals_by_species(grouped_data)
     print(obtained)
     expected_species_1 = 5
     assert obtained.loc["species 1"].values == expected_species_1
+    expected_species_2 = 1
+    assert obtained.loc["species 2"].values == expected_species_2
