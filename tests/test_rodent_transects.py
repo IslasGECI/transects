0c96de421bd8b0255c292846add03e886472b837
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-23 20:51:08 +0000

    🛑🧪 Fail tests

--- tests/test_rodent_transects.py
+++ tests/test_rodent_transects.py
@@ -1,10 +1,11 @@
-from transects import count_rodent_captures_by_site
+from transects import count_rodent_captures_by_site, calculate_rodent_effort_by_site
 
 import pandas as pd
 
+traps_status_data = pd.read_csv("tests/data/rodent_captures.csv")
+
 
 def test_count_rodent_captures_by_site():
-    traps_status_data = pd.read_csv("tests/data/rodent_captures.csv")
     obtained = count_rodent_captures_by_site(traps_status_data)
     expected_columns = ["Sitio", "captures"]
     assert obtained.columns in expected_columns
@@ -12,3 +13,9 @@ def test_count_rodent_captures_by_site():
     obtained_captures = obtained.loc["Laguna del Toro", "captures"]
     expected_captures = 1
     assert obtained_captures == expected_captures
+
+
+def test_calculate_rodent_effort_by_site():
+    obtained = calculate_rodent_effort_by_site(traps_status_data)
+    expected_columns = ["Sitio", "effort"]
+    assert obtained.columns in expected_columns
