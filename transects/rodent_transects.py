6d7f3b0917670f37cb5e9cdffd4fa97d3724f7f6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-23 18:37:57 +0000

    ♻️  Refactor

--- transects/rodent_transects.py
+++ transects/rodent_transects.py
@@ -1,11 +1,10 @@
 def count_rodent_captures_by_site(traps_status_data):
     identify_captures(traps_status_data)
-    grouped_count = traps_status_data.groupby(by=["Sitio"])[["status_trampa"]].sum()
-    return grouped_count.rename(columns={"status_trampa": "captures"})
+    return traps_status_data.groupby(by=["Sitio"])[["captures"]].sum()
 
 
 def identify_captures(traps_status_data):
     is_not_capture = traps_status_data["status_trampa"] != "Captura"
-    traps_status_data.loc[is_not_capture, "status_trampa"] = 0
+    traps_status_data.loc[is_not_capture, "captures"] = 0
     is_capture = traps_status_data["status_trampa"] == "Captura"
-    traps_status_data.loc[is_capture, "status_trampa"] = 1
+    traps_status_data.loc[is_capture, "captures"] = 1
