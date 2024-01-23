e55e7cb7f6eebc8e266b3a82cf0dd50761b70044
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-23 20:42:26 +0000

    ♻️  Refactor

--- tests/test_rodent_transects.py
+++ tests/test_rodent_transects.py
@@ -8,3 +8,7 @@ def test_count_rodent_captures_by_site():
     obtained = count_rodent_captures_by_site(traps_status_data)
     expected_columns = ["Sitio", "captures"]
     assert obtained.columns in expected_columns
+
+    obtained_captures = obtained.loc["Laguna del Toro", "captures"]
+    expected_captures = 1
+    assert obtained_captures == expected_captures
