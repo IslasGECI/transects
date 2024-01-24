e2da8bd91c3b08128bfe9504f9d943f5851e0656
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-24 21:32:11 +0000

    ♻️  Refactor

--- transects/cli.py
+++ transects/cli.py
@@ -1,9 +1,15 @@
 from transects import get_density_by_specie
+
 import typer
+from typing_extensions import Annotated
 
 cli = typer.Typer()
 
 
 @cli.command()
-def write_bird_densities(bird_transects: str = "", bird_records: str = "", output_path: str = ""):
+def write_bird_densities(
+    bird_transects: Annotated[str, typer.Option()],
+    bird_records: Annotated[str, typer.Option()],
+    output_path: Annotated[str, typer.Option()],
+):
     get_density_by_specie(bird_records, bird_transects).to_csv(output_path)
