793f3e22c3d04fcd0baa6ea40c5366ac2b660cad
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-24 19:43:11 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,7 @@ from transects import cli
 
 
 from typer.testing import CliRunner
+import os
 
 
 transect_path = "tests/data/bird_transects.csv"
@@ -13,3 +14,19 @@ runner = CliRunner()
 def test_write_bird_densities():
     result = runner.invoke(cli, ["write-bird-densities", "--help"])
     assert result.exit_code == 0
+
+    output_path = "tests/bird_densities.csv"
+    result = runner.invoke(
+        cli,
+        [
+            "write-bird-densities",
+            "--bird-transects",
+            transect_path,
+            "--bird-records",
+            bird_records_path,
+            "--output-path",
+            output_path,
+        ],
+    )
+
+    assert os.path.exists(output_path)
