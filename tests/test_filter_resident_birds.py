a743f350e6c8eb4e9ec345183f6a6bf92c83b244
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-01-26 19:43:57 +0000

    ✅ Pass tests

--- tests/test_filter_resident_birds.py
+++ tests/test_filter_resident_birds.py
@@ -1,4 +1,4 @@
-from transects import filter_resident_birds
+from transects import filter_resident_birds, filter_resident_records
 
 import pandas as pd
 
@@ -11,6 +11,24 @@ def test_filter_resident_birds():
         }
     )
     obtained = filter_resident_birds(observed_list_df)
-    print(obtained)
     expected_residents = "Anas discors"
     assert obtained.values[0] == expected_residents
+
+
+def test_filter_resident_records():
+    records_list_df = pd.DataFrame(
+        {
+            "Especie": [
+                "Calidris alba",
+                "Calidris alba",
+                "Anas discors",
+                "Columbina passerina",
+                "Anas discors",
+            ],
+            "n_individuos": [5, 3, 8, 2, 2],
+        }
+    )
+    resident_birds_df = pd.Series({"Especie": ["Anas discors"]})
+    obtained = filter_resident_records(resident_birds_df, records_list_df)
+    expected_rows = 2
+    assert len(obtained) == expected_rows
--- transects/filter_resident_birds.py
+++ transects/filter_resident_birds.py
@@ -1,2 +1,6 @@
 def filter_resident_birds(observed_list_df):
     return observed_list_df[observed_list_df.residentes]["Especie"]
+
+
+def filter_resident_records(residents_birds_df, records_list_df):
+    return [1, 2]
