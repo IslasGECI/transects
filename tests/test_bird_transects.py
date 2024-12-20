892eeb1354cf0433fb008b45785df1a30080967a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-20 18:26:08 +0000

    ♻️  Refactor

--- tests/test_bird_transects.py
+++ tests/test_bird_transects.py
@@ -4,7 +4,7 @@ from pytest import approx
 from transects import (
     count_by_specie_and_method,
     xxcount_total_individuals_by_species,
-    get_density_by_specie,
+    get_density_by_specie_and_day,
     get_density_by_species_and_transects,
     get_total_area,
     get_transect_area,
@@ -28,15 +28,18 @@ def test_get_density_by_species_and_transects():
 
 
 def test_get_density_by_specie():
-    obtained = get_density_by_specie(bird_records_df, transect_df)
+    obtained = get_density_by_specie_and_day(bird_records_df, transect_df)
     expected_columns = 2
     assert len(obtained.columns) == expected_columns
     assert (
         approx(obtained.loc["Setophaga pitiayumii insularis", "densidad"].sum(), abs=1e-4)
         == 53 / 23.6474
     )
+    print(obtained.loc["Setophaga pitiayumii insularis"])
     obtained_density_by_day = obtained.loc["Setophaga pitiayumii insularis", "16/11/2023"].densidad
     assert approx(obtained_density_by_day, abs=1e-4) == 21 / 23.6474
+    # assert approx(
+    #     obtained.loc["Setophaga pitiayumii insularis"].densidad, abs=1e-4) == 13.25 / 23.6474
 
 
 def test_get_transect_area():
--- transects/bird_transects.py
+++ transects/bird_transects.py
@@ -12,7 +12,7 @@ def get_density_by_species_and_transects(bird_records_df, transect_df):
     return joined
 
 
-def get_density_by_specie(bird_records_df, transects_info_df):
+def get_density_by_specie_and_day(bird_records_df, transects_info_df):
     counts = xxcount_total_individuals_by_species(bird_records_df).to_frame()
     total_area = get_total_area(transects_info_df)
     counts["densidad"] = counts.n_individuos / total_area
