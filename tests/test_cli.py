33df5bc706559cac9ea14edb5f5e90d939acf999
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-11-03 21:06:47 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -88,6 +88,34 @@ def test_write_rodent_trapping_success():
     os.remove(output_path)
 
 
+def test_write_selected_bird_records():
+    result = runner.invoke(cli, ["write-selected-bird-records", "--help"])
+    assert result.exit_code == 0
+
+    output_path = "tests/terrestrial_selected_bird_records.csv"
+    observed_path = "tests/data/observed_bird_species.csv"
+
+    if os.path.exists(output_path):
+        os.remove(output_path)
+
+    result = runner.invoke(
+        cli,
+        [
+            "write-selected-bird-records",
+            "--observed-birds",
+            observed_path,
+            "--bird-records",
+            bird_records_path,
+            "--group",
+            "terrestrial",
+            "--output-path",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+    assert os.path.exists(output_path)
+
+
 def test_write_resident_bird_records():
     result = runner.invoke(cli, ["write-resident-bird-records", "--help"])
     assert result.exit_code == 0
