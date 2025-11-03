707e7d372d95dba330a30724766509f9818471a5
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-11-03 22:58:13 +0000

    ♻️  Refactor

--- transects/bird_transects.py
+++ transects/bird_transects.py
@@ -24,11 +24,6 @@ def get_density_by_species_and_transects(bird_records_df, transect_df):
     return joined
 
 
-def get_mean_density_by_species(bird_records_df, transect_df):
-    density_by_day = get_density_by_species_and_day(bird_records_df, transect_df)
-    return density_by_day.groupby(level="Especie").agg({"n_individuos": "sum", "densidad": "mean"})
-
-
 def xxget_mean_density_by_species(bird_records_df, transect_df):
     density_by_day = xxget_density_by_species_and_day(bird_records_df, transect_df)
     return density_by_day.groupby(level="species_level_name").agg(
