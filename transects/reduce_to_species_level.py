d65153b28c01c61bab382ea2a01041504f687818
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-11-03 22:24:08 +0000

    ✅ Pass tests

--- transects/reduce_to_species_level.py
+++ transects/reduce_to_species_level.py
@@ -1,2 +1,12 @@
+import pandas as pd
+
+
+def add_species_level_column(bird_records_df: pd.DataFrame) -> pd.DataFrame:
+    bird_records_df["species_level_name"] = bird_records_df["Especie"].apply(
+        reduce_to_species_level
+    )
+    return bird_records_df
+
+
 def reduce_to_species_level(bird_name: str) -> str:
     return " ".join(bird_name.split(" ")[:2])
