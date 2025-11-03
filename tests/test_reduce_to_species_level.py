0f21e97a715679ed79d46582b8fcdd45135e076c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-11-03 22:23:15 +0000

    🛑🧪 Fail tests

--- tests/test_reduce_to_species_level.py
+++ tests/test_reduce_to_species_level.py
@@ -1,4 +1,21 @@
-from transects.reduce_to_species_level import reduce_to_species_level
+from transects.reduce_to_species_level import add_species_level_column, reduce_to_species_level
+
+
+import pandas as pd
+
+
+def test_add_species_level_column():
+    bird_records_df = pd.DataFrame(
+        {
+            "Especie": [
+                "Amazona oratrix tresmariae",
+                "Amazona oratrix",
+                "Leptotila verreauxi capitalis",
+            ]
+        }
+    )
+    obtained = add_species_level_column(bird_records_df)
+    assert "species_level_name" in obtained.columns
 
 
 def test_reduce_to_species_level():
