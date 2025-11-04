b02a74aa76587020b2c990990ba6e82453a86a33
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-11-04 17:37:24 +0000

    ♻️  Refactor

--- transects/bird_transects.py
+++ transects/bird_transects.py
@@ -13,19 +13,18 @@ def get_mean_density_by_species_and_transects(bird_records_df, transect_df):
         ["clave_muestreo", "species_level_name"]
     ).agg({"n_individuos": "sum", "area": "mean", "density": "mean"})
 
-    mean_densities_by_species_and_transect_sorted_by_transect = (
+    mean_densities_by_species_and_transect_sorted_by_species = (
         mean_densities_by_species_and_transect.sort_values(
             ["clave_muestreo", "n_individuos"], ascending=False
         )
     )
     total_individuals_by_transect = (
-        mean_densities_by_species_and_transect_sorted_by_transect.groupby(level="clave_muestreo")[
+        mean_densities_by_species_and_transect_sorted_by_species.groupby(level="clave_muestreo")[
             "n_individuos"
         ].sum()
     )
-
     sorted_transects_by_totals = total_individuals_by_transect.sort_values(ascending=False).index
-    return mean_densities_by_species_and_transect_sorted_by_transect.loc[sorted_transects_by_totals]
+    return mean_densities_by_species_and_transect_sorted_by_species.loc[sorted_transects_by_totals]
 
 
 def get_density_by_species_and_transects(bird_records_df, transect_df):
