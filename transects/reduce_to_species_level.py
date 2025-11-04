efa87818c2689e2f75a10048d8f6425b0f311cf4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-11-04 16:30:07 +0000

    ♻️  Refactor

--- transects/reduce_to_species_level.py
+++ transects/reduce_to_species_level.py
@@ -2,10 +2,11 @@ import pandas as pd
 
 
 def add_species_level_column(bird_records_df: pd.DataFrame) -> pd.DataFrame:
-    bird_records_df["species_level_name"] = bird_records_df["Especie"].apply(
+    bird_records_df_copy = bird_records_df.copy()
+    bird_records_df_copy["species_level_name"] = bird_records_df_copy["Especie"].apply(
         reduce_to_species_level
     )
-    return bird_records_df
+    return bird_records_df_copy
 
 
 def reduce_to_species_level(bird_name: str) -> str:
